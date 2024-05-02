e30a5c7a354d2bfe688afcdea4923f7a9cadc91a
Trell <144071009+Izaman5@users.noreply.github.com>
2024-05-02 18:28:06 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,47 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "5.25.0"
+      version = "5.27.0"
     }
   }
 }
-
 provider "google" {
-  # Configuration options
-  region = "asia-northeast1"
-  project ="astral-option-419300"
-  zone = "asia-northeast1-a"
-  credentials = *****
+# Configuration options
+project = "********"
+region = "asia-northeast1"
+zone = "asia-northeast1-a"
+credentials = "*******.json"
+}
+#make bucket
+resource "google_storage_bucket" "GBB" {
+name = "get_buckets_bucket"
+location = "asia-northeast1"
+storage_class = "STANDARD"
+force_destroy = true
 
 }
+resource "google_storage_bucket_access_control" "public_rule" {
+  bucket = google_storage_bucket.GBB.name
+  role   = "READER"
+  entity = "allUsers"
+}
+
+
+resource "google_storage_bucket_object" "picture" {
+  name ="bm.jpg"
+  source = "bm.jpg"
+    bucket =  google_storage_bucket.GBB.name
+    content_type = "jpg"
+}
+
+
+
+resource "google_storage_bucket_object" "index" {
+name = "index.html"  
+source = "index.html"
+bucket =  google_storage_bucket.GBB.name
+content_type = "text/html"
+}
+
+
+
